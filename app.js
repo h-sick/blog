48ae20fd661b5d0a92f678a3ce8dc71f1cc7b899
Hyosik Kim <luna23825@gmail.com>
2025-01-28 20:38:35 +1100

    feat: set view engine and views

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
