7c9ab3220bbe76a67d4343fa93fce66f8e37805b
Hyosik Kim <luna23825@gmail.com>
2025-02-13 23:31:07 +1100

    feat: change User model attributes

--- models/user.js
+++ models/user.js
@@ -13,10 +13,38 @@ const User = sequelize.define('user', {
   email: {
     type: Sequelize.STRING,
     allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
   },
   password: {
     type: Sequelize.STRING,
     allowNull: false,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  // isVerified: {
+  //   type: Sequelize.BOOLEAN,
+  //   defaultValue: false
+  // },
+  // verificationToken: {
+  //   type: Sequelize.STRING,
+  // },
+  // resetPasswordToken: {
+  //   type: Sequelize.STRING,
+  // },
+  // resetPasswordExpires: {
+  //   type: Sequelize.DATE,
+  // },
+  loginAttempts: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+  },
+  lockUntil: {
+    type: Sequelize.DATE,
   },
 });
 
