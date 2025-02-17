3917d0a72e620916bffa506d12c4b0b2a5a0060e
Hyosik Kim <luna23825@gmail.com>
2025-02-17 23:35:37 +1100

    refactor: remove JAWSDB_URL

--- database.js
+++ database.js
@@ -3,19 +3,19 @@ const Sequelize = require('sequelize');
 let sequelize;
 
 // production
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  // local
-  sequelize = new Sequelize(
-    process.env.SEQUELIZE_DATABASE,
-    process.env.SEQUELIZE_USER,
-    process.env.SEQUELIZE_PASSWORD,
-    {
-      dialect: 'mysql',
-      host: process.env.SEQUELIZE_HOST,
-    }
-  );
-}
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+// local
+sequelize = new Sequelize(
+  process.env.SEQUELIZE_DATABASE,
+  process.env.SEQUELIZE_USER,
+  process.env.SEQUELIZE_PASSWORD,
+  {
+    dialect: 'mysql',
+    host: process.env.SEQUELIZE_HOST,
+  }
+);
+// }
 
 module.exports = sequelize;
